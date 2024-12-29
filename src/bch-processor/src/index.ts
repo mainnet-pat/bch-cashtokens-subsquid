7f735cd09b3060162bdcadb46c67b9e68b8d5d31
mainnet-pat <mainnet_pat@protonmail.com>
2024-12-29 09:10:36 +0000

    Extract bch-process into squid-sdk

--- src/bch-processor/src/index.ts
+++
@@ -1,4 +0,0 @@
-export {assertNotNull} from '@subsquid/util-internal'
-export {toHex, decodeHex} from '@subsquid/util-internal-hex'
-export * from './processor.js'
-export * from './interfaces/data.js'
